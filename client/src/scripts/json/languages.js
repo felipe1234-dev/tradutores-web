3745255943f2473522997c6842e26f5b0c6246b9
felipe1234-dev <felipejean2002@gmail.com>
2021-10-13 02:20:26 -0300

    Create languages.js

---
+++ client/src/scripts/json/languages.js
@@ -0,0 +1,18 @@
+import brazil from 'src/media/brazil.svg';
+import france from 'src/media/france.svg';
+import germany from 'src/media/germany.svg';
+import italy from 'src/media/italy.svg';
+import spain from 'src/media/spain.svg';
+import usa from 'src/media/usa.svg';
+
+const languages = [
+    { label: '---',        value: '---',        src: '---'   },
+    { label: 'portuguese', value: 'portuguese', src: brazil  }, 
+    { label: 'french',     value: 'french',     src: france  }, 
+    { label: 'german',     value: 'german',     src: germany }, 
+    { label: 'italian',    value: 'italian',    src: italy   },  
+    { label: 'spanish',    value: 'spanish',    src: spain   },  
+    { label: 'english',    value: 'english',    src: usa     },  
+];
+
+export default languages;
