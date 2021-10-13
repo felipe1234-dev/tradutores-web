46ab169f8b25059b03f33188b7506a212c9b761f
felipe1234-dev <felipejean2002@gmail.com>
2021-10-13 02:20:46 -0300

    Create topics.js

---
+++ client/src/scripts/json/topics.js
@@ -0,0 +1,60 @@
+const topics = [
+    {   
+        label: "Other",
+        value: "Other"
+    },
+    {   
+        label: "Literature",
+        value: "Literature"
+    },
+    {   
+        label: "Science",
+        value: "Science"   
+    },
+    {
+        label: "History",
+        value: "History"
+    },
+    {   
+        label: "Tech",
+        value: "Tech",    
+    },
+    {   
+        label: "Health",
+        value: "Health" 
+    },
+    {   
+        label: "News",
+        value: "News" 
+    },
+    {
+        label: "Politics",
+        value: "Politics"
+    },
+    {   
+        label: "Art",
+        value: "Art"     
+    },
+    {   
+        label: "Sports",
+        value: "Sports"
+    },
+    {   
+        label: "Education",
+        value: "Education",
+    },
+    {   
+        label: "Music",
+        value: "Music", 
+    }, 
+    {   
+        label: "Philosophy",
+        value: "Philosophy",
+    },
+    {   
+        label: "Religion",
+        value: "Religion",
+    }
+]
+
+export default topics;
