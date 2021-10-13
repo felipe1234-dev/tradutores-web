837ef681c94a83e670b001eaab69e5cf3ec25ea3
felipe1234-dev <felipejean2002@gmail.com>
2021-10-13 02:20:13 -0300

    Create filters.js

---
+++ client/src/scripts/json/filters.js
@@ -0,0 +1,33 @@
+const filters = {
+    progress: [
+        {   
+            label: 'Needs to be translated',
+            value: 'Needs to be translated'      
+        },
+        {   
+            label: 'Needs to be checked',
+            value: 'Needs to be checked'      
+        },
+        {   
+            label: 'Finished',
+            value: 'Finished'      
+        }
+    ],
+    /* -------------------------------------- */
+    level: [
+        {   
+            label: 'Easier',
+            value: 'Easier'      
+        },
+        {   
+            label: 'Medium',
+            value: 'Medium'      
+        },
+        {   
+            label: 'Harder',
+            value: 'Harder'
+        }
+    ]
+}
+
+export default filters;
